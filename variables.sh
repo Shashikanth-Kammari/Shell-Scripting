fb8b2260dfb95343e3f0db281f343c6435e67244
Shashikanth Kammari <https://github.com/Shashikanth-Kammari>
2025-03-17 20:55:43 +0530

    change

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read name=Suresh
-read name=Ramesh
+read name=$Suresh
+read name=$Ramesh
 
 echo "Hi $Suresh , How are you"
 echo "Hi $Ramesh , I'm doing well! what abouit you $Suresh"
