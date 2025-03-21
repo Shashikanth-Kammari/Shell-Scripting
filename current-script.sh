287be0118892f799b5cbe487b03b563046179fd2
Shashikanth Kammari <https://github.com/Shashikanth-Kammari>
2025-03-21 22:52:45 +0530

    pushed

--- current-script.sh
+++ current-script.sh
@@ -4,6 +4,6 @@ COURSE="Devops from Current Script"
 echo "Before calling other script, Course : $COURSE"
 echo "Process ID of current shell script : $$"
 #source ./other-script.sh
-source ./other-script.sh
+dash ./other-script.sh
 echo "After calling other script, Course : $COURSE"
 
