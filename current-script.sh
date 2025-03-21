c4a592dba9ce855d452c08af7bfc1aed12b4a942
Shashikanth Kammari <https://github.com/Shashikanth-Kammari>
2025-03-21 22:11:49 +0530

    Otherscript

--- current-script.sh
+++ current-script.sh
@@ -3,8 +3,6 @@
 COURSE="Devops from Current Script"
 echo "Before calling other script, Course : $COURSE"
 echo "Process ID of current shell script : $$"
-
 ./other-script.sh
-
 echo "After calling other script, Course : $COURSE"
 
